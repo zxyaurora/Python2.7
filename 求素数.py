928396caee67aaff537872707b448e1e6c689169
zxyaurora <503656462@qq.com>
2017-10-11 11:21:52 +0800

    Update 求素数.py

--- 求素数.py
+++ 求素数.py
@@ -4,5 +4,14 @@
 求素数的算法：
 如果n能够整除n的平方根，则n就是素数。
 """
-def prime(n):
+def is_prime(n):
+    if n<=1 :
+    	  return False
+    for i in range(2,int(math.sqrt(n)+1)):
+        if n % i == 0:
+        	  return False
+    return True
     
+if __name__ == "__main__":
+	  primes = [i for i in range(2,100) if is_prime(i)]
+	  print primes
